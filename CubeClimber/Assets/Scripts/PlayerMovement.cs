5ba1de465b18d8429d8446f94dda23e283d6645e
omellen2269 <olivia.mellen@live.com>
2021-10-22 13:34:22 -0500

    added restart when fall and fireworks

--- CubeClimber/Assets/Scripts/PlayerMovement.cs
+++ CubeClimber/Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody rigid;
     public bool cubeOnGround = true;
     public GameObject endScreen;
+    public GameObject fireworks;
+    public Transform player;
 
     private void FixedUpdate()
     {
-
+        //movement
         if (Input.GetKey("d"))
         {
             rigid.AddForce(500f * Time.deltaTime, 0, 0);
@@ -21,14 +23,21 @@ public class PlayerMovement : MonoBehaviour
             rigid.AddForce(-500f * Time.deltaTime, 0, 0);
         }
 
+        //jump mechanism
         if(Input.GetKey("w") && cubeOnGround == true)
         {
             rigid.AddForce(new Vector3(0, 500f * Time.deltaTime, 0), ForceMode.Impulse);
             cubeOnGround = false;
         }
 
+        //player restart when dies
+        if(player.position.y <= 0)
+        {
+            player.SetPositionAndRotation(new Vector3(4f, 5.25f, -1.25f), new Quaternion(0, 0, 0, 0));
+        }
   
     }
+    //hits platform
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Platform")
@@ -36,10 +45,11 @@ public class PlayerMovement : MonoBehaviour
             cubeOnGround = true;
         }
 
-	if(collision.gameObject.name == "PlatformFinal")
-	{
-	    endScreen.gameObject.SetActive(true);
-	}
+	    if(collision.gameObject.name == "PlatformFinal")
+	    {
+	        endScreen.gameObject.SetActive(true);
+            fireworks.gameObject.SetActive(true);
+	    }
     }
 
 }
